ddcc027806861ceeaffe77b6b59fd8b815ef452b
malik016 <63663632+malik9153@users.noreply.github.com>
2022-12-09 21:15:52 +0000

    hello

--- myApp.js
+++ myApp.js
@@ -2,7 +2,9 @@ let express = require("express");
 let app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello Express");
+    absolutePath = __dirname + "/views/index.html"
+  res.send(absolutePath);
 });
+
 
 module.exports = app;
