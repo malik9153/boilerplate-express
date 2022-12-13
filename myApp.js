81afa8368fda833cda48d5f47aaea2492f54bf0c
malik016 <63663632+malik9153@users.noreply.github.com>
2022-12-13 20:39:37 +0000

    yoo

--- myApp.js
+++ myApp.js
@@ -1,9 +1,8 @@
 let express = require("express");
 let app = express();
 
-app.get("/", (req, res) => {
-    absolutePath = __dirname + "/views/index.html"
-  res.sendFile(absolutePath);
+app.get("/json", (req, res) => {
+    res.json({ message: "Hello json" });
 });
 
 
