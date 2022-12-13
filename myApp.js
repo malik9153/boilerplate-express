cd25505cbb4b322ad95aeec8f6990e668fef8c0c
malik016 <63663632+malik9153@users.noreply.github.com>
2022-12-13 20:57:08 +0000

    yooo

--- myApp.js
+++ myApp.js
@@ -1,9 +1,10 @@
 let express = require("express");
 let app = express();
 require("dotenv").config();
+console.log(process.env["MESSAGE_STYLE"]);
 let phrase = ""
 app.get("/json", (req, res) => {
-  if(process.env['MESSAGE_STYLE'] === "uppercase"){
+  if(process.env['MESSAGE_STYLE'] === uppercase){
     phrase = "HELLO JSON"
   }
   else{
