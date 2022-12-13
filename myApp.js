6ab7f76bebca3f934a7eafeedfdcf53296712f11
malik016 <63663632+malik9153@users.noreply.github.com>
2022-12-13 20:55:46 +0000

    yooo

--- myApp.js
+++ myApp.js
@@ -1,8 +1,15 @@
 let express = require("express");
 let app = express();
 require("dotenv").config();
+let phrase = ""
 app.get("/json", (req, res) => {
-  res.json({ message: "Hello json" });
+  if(process.env['MESSAGE_STYLE'] === "uppercase"){
+    phrase = "HELLO JSON"
+  }
+  else{
+    phrase = "Hello json"
+  }
+  res.json({ message: phrase });
 });
 
 module.exports = app;
